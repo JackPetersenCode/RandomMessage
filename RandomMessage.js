61e805a50edb85e63918714a626643c083c50f29
Jack <jackpetersenmusic@gmail.com>
2021-05-18 16:19:54 -0700

    Added a comment explaining the expected function and output of the program.

---
+++ RandomMessage.js
@@ -0,0 +1,3 @@
+//Program prints out a random message. We will have three different properties with a list of strings for each.
+//The program will select three random items and place them into a random
+//sentence like in a mad-lib. There will be a couple different sentence outlines.
