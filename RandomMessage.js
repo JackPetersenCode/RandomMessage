cdafa3a73102c827e9e71c4e7b8fe825e3164fc0
Jack <jackpetersenmusic@gmail.com>
2021-05-18 22:02:08 -0700

    Whatever

--- RandomMessage.js
+++ RandomMessage.js
@@ -10,12 +10,30 @@
 //random numbers and print out the random sentence with the random words inserted in.
 
 
-const RandomFactory = function() {
-    let item1 = ['whistles', 'pickles', 'sugar', 'muffins', 'potatoes']
-    let item2 = ['chicken', 'anteloupe', 'bird', 'worm', 'bear', 'hornet']
-    let item3 = ['Beaverton', 'San Luis Obispo', 'New York', 'Green Bay', 'Memphis', 'Atlanta']
-    let rando1 = Math.floor(Math.random() * item1.length)
-    let rando2 = Math.floor(Math.random() * item2.length)
-    let rando3 = Math.floor(Math.random() * item3.length)
-11
+const MessageObj = {
+    item1: ['whistles', 'pickles', 'sugar', 'muffins', 'potatoes'],
+    item2: ['chicken', 'anteloupe', 'bird', 'worm', 'bear', 'hornet'],
+    item3: ['Beaverton', 'San Luis Obispo', 'New York', 'Green Bay', 'Memphis', 'Atlanta'],
+    makeMessage() {
+        let rando1 = Math.floor(Math.random() * item1.length)
+        let rando2 = Math.floor(Math.random() * item2.length)
+        let rando3 = Math.floor(Math.random() * item3.length)
+        let thing1 = this.item1[rando1]
+        let thing2 = this.item2[rando2]
+        let thing3 = this.item3[rando3]
+        let randNum = Math.floor(Math.random() * 3)
+        switch (randNum) {
+            case 0:
+                console.log(`The ${thing1} was very big, almost too big. It got in the way \n
+                of the ${thing2}. This even occured in real time, in ${thing3}`)
+            case 1:
+                console.log(`You will do well with ${thing1}. The body cam footage showed \n
+                ${thing2}. Your future in ${thing3} looks bright.`)
+            case 2:
+                console.log(`Bad news, your ${thing2} was in the way. The ${thing1} was bad for \n
+                your fortune, try again in ${thing3}.`)
+            default:
+                console.log(`Why didn't this work.`)
+        }
+    }
 }
